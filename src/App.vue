6fe1c4895b640e5f8b3a3278b4be320f7616e36b
NeuzaSousa <neuzarssousa@gmail.com>
2022-03-12 10:29:48 +0100

    Inserted image

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Hello world</h1>
+    <hello-world />
+    <instagram-login />
+    <img :src="image"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    /*import image from "./assets/logo.png"
+    
+    export default {
+        data: function () {
+            return {
+                image: image
+            }
+        }
+    }*/
+
+  import helloWorld from "./components/HelloWorld";
+  import instagramLogin from "./components/instagramLogin";
+  import image from "./image.jpg"
+  export default {
+    name: 'App',
+    components: {
+       helloWorld,
+      instagramLogin
+    },
+    data: function(){
+      return {
+        image: image
+      }
+    }
+  };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: grid;
+  /*background-color: black;*/
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
+  
+}
+body{
+  background-color: #FAFAFA;
 }
 </style>
---
+++ src/components/instagramLogin.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Hello from login</h1>
+        <h2>The count is: {{ count }}</h2>
+        <button @click="count++">+1</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count: 0
+        };
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
