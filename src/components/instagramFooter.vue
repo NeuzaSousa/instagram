af4769b40162abed99abd1857abb0ffa6c26bcc8
NeuzaSousa <neuzarssousa@gmail.com>
2022-03-13 16:37:03 +0100

    Added more styling

--- src/components/instagramFooter.vue
+++ src/components/instagramFooter.vue
@@ -3,38 +3,41 @@
             <p><a target="_blank" href="https://about.facebook.com/meta">Meta</a><a target="_blank" href="https://about.instagram.com/">About</a><a target="_blank" href="https://about.instagram.com/blog">Blog</a><a href="https://about.instagram.com/about-us/careers">Jobs</a><a target="_blank" href="https://help.instagram.com/">Help</a><a target="_blank" href="https://developers.facebook.com/docs/instagram">API</a><a href="https://help.instagram.com/519522125107875/?maybe_redirect_pol=0">Privacy</a>
             <a href="https://help.instagram.com/581066165581870">Impressum/Terms/NetzDG/UrhDaG</a><a href="https://www.instagram.com/directory/profiles/">Top Accounts</a><a href="https://www.instagram.com/directory/hashtags/">Hashtags</a><a href="https://www.instagram.com/explore/locations/">Locations</a></p>
             <p><a href="https://www.instagram.com/topics/dance-and-performance/">Dance</a><a href="https://www.instagram.com/topics/food-and-drink/">Food & Drink</a><a href="https://www.instagram.com/topics/home-and-garden/">Home & Garden</a><a href="https://www.instagram.com/topics/music/">Music</a><a href="https://www.instagram.com/topics/visual-arts/">Visual Arts</a></p>
-            <p><button>{{ language }}</button>© 2022 Instagram from Meta</p></footer>
-        <div v-if="active">
-            Menu
-        </div>
-
+            <p><button>{{ language }}</button>© 2022 Instagram from Meta</p>
+    </footer>
 </template>
 
 <script>
+    //import dropdownLanguage from "./dropdownLanguage";
     export default{
         data() {
             return{
-            message: "Barcelona"
-        };
-  }
+                language: "English",
+            };
+        },
     }
+
 </script>
 
 <style scoped>
     a {
-    color: #9B8E8E;
-    font-weight: bold;
-    font-size: 11px;
-    font-family: 'Roboto', sans-serif;
-    padding: 10px;
-    text-decoration: none;
-  }
+        color: #9B8E8E;
+        font-weight: bold;
+        font-size: 12px;
+        font-family: 'Roboto', sans-serif;
+        padding: 10px;
+        text-decoration: none;
+        font-weight: 300;
+    }
+
     p {
-    color: #9B8E8E;
-    font-weight: bold;
-    font-size: 11px;
-    font-family: 'Roboto', sans-serif;
-    padding: 10px;
+        color: #9B8E8E;
+        font-weight: bold;
+        font-size: 12px;
+        font-family: 'Roboto', sans-serif;
+        padding: 10px;
+        text-align: center;
+        font-weight: 300;
     }
 
 </style>
--- src/components/instagramLogin.vue
+++ src/components/instagramLogin.vue
@@ -4,32 +4,34 @@
             <h1 id="title">Instagram</h1>
             <input type="text" call="input" v-model="username" placeholder="Phone number, username, or email" />
             <input type="password" call="input" v-model="password" placeholder="Password" />
-            <button @click="login">Log In</button>
-            <p><span id="hr">&#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240;</span>&#10240;&#10240;OR&#10240;&#10240;<span id="hr">&#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240;</span></p>
-            <p><img :src="facebook" id="facebook"><a href="">Login with Facebook</a></p>
-            <a href="">Forgot password?</a>
-            <p>You can also <a href="https://help.instagram.com/contact/543840232909258">report content you believe is unlawful</a> under the Network Enforcement Act (NetzDG) without logging in.</p>
+            <button @click="login" id="loginButton">Log In</button>
+            <p id="or"><span id="hr">&#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240;</span>&#10240;&#10240;OR&#10240;&#10240;<span id="hr">&#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240; &#10240;</span></p>
+            <p><img :src="facebook" id="facebookImg"><a id="facebookLogin" href="https://www.instagram.com/fxcal/disclosure/?next=%2F">Log in with Facebook</a></p>
+            <a id="forgotPassword" href="https://www.instagram.com/accounts/password/reset/">Forgot password?</a>
+            <p id="netzDG">You can also <a href="https://help.instagram.com/contact/543840232909258">report content you believe is unlawful</a> under the Network Enforcement Act (NetzDG) without logging in.</p>
         </div>
         <div id="sign-up">
-            Don't have an account? <a href="">Sign up</a>
+            <p>Don't have an account? <a href="https://www.instagram.com/accounts/emailsignup/">Sign up</a></p>
         </div>
-        <p>Get the app</p>
-        <button><a href=""></a>Download on the App Store</button>
-        <button @click="toggle"><a href=""></a>Get it on Google Play</button>
+        <p id="getApp">Get the app.</p>
+        <button class="black"><a target="_blank" href="https://apps.apple.com/app/instagram/id389801252?vt=lo"><img :src="apple" class="icon" /><span id="firstLineApple">Download on the</span><br><span id="secondLineApple">App Store</span></a></button>
+        <button class="black"><a target="_blank" href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb&utm_campaign=loginPage&ig_mid=7A0CC0B8-4420-4882-A51B-965D4A590A1A&utm_content=lo&utm_medium=badge"><img :src="play" class="icon" /><span id="firstLinePlay">GET IT ON</span><br><span id="secondLinePlay">Google Play</span></a></button>
         
     </div>
 </template>
 
 <script>
     import facebook from "./facebook.png"
+    import apple from "./apple.jpg"
+    import play from "./play.jpg"
     export default{
         data(){
             return{
                 username: "",
                 password: "",
                 facebook: facebook,
-                language: "English",
-                active: false
+                apple: apple,
+                play: play,
             };
         },
 
@@ -42,9 +44,7 @@
                 }
             },
 
-            toggle() {
-                this.active = !this.active
-            }
+
         }
     };
 </script>
@@ -55,11 +55,13 @@
         background-color: white;
         border: #DBDBDB 1px solid;
         width: 345px; 
+        margin-bottom: 10px;
     }
     #title {
         font-family: 'Grand Hotel', cursive;
         font-size: 55px;
         text-align: center;
+        font-weight: 200;
     }
 
     input {
@@ -72,7 +74,7 @@
         border-radius: 3px;
     }
 
-    button {
+    #loginButton {
         width: 260px;
         margin-left: 40px;
         padding: 7px;
@@ -86,29 +88,135 @@
 
     ::placeholder {
         color: #9B8E8E;
-        font-size: 12px;
+        font-size: 11px;
     }
 
-    #facebook {
-        width: 15px;
+    #facebookImg {
+        width: 17px;
+        padding: 10px;
+        position: relative;
+        left: 80px;
     }
 
     #sign-up {
         background-color: white;
         border: #DBDBDB 1px solid; 
-        width: 345px; 
+        width: 345px;
+        color: #632663;
+        text-align: center;
+        font-family: "Roboto", sans-serif;
+        font-size: 13.8px;
+        letter-spacing: 0.5px;
+        padding: 8px 0;
+    }
+
+    #sign-up a{
+        text-decoration: none;
+        color:#3eb0fc;
+        font-weight: bold;
     }
 
     #hr {
         text-decoration: line-through;
     }
 
-    p {
+    #or {
         text-align: center;
         font-family: 'Roboto', sans-serif;
         font-size: 12px;
         font-weight: bold;
         color: #9B8E8E;
+    }
+
+    #facebookLogin {
+        color: #3B5998;
+        font-size: 12.5px;
+        font-family: 'Roboto', sans-serif;
+        text-decoration: none;
+        position: relative;
+        top: -13px;
+        left: 80px;
+        font-weight: bold;
+        letter-spacing: 0.5px;
+    }
+
+    #forgotPassword {
+        color: rgb(61, 61, 61);
+        font-size: 12px;
+        font-family: 'Roboto', sans-serif;
+        text-decoration: none;
+        position: relative;
+        left: 130px;
+    }
+
+    #netzDG {
+        font-family: sans-serif;
+        font-weight: 100;
+        width: 280px;
+        font-size: 12px;
+        margin: auto;
+        padding: 30px 0 30px 0;
+        line-height: 1.5;
+        color: #9B8E8E;
+        text-align: center;
+    }
+
+    #netzDG a {
+        text-decoration: none;
+        color:black;
+    }
+
+    #getApp {
+        font-family: "Roboto", sans-serif;
+        font-weight: 300;
+        font-size: 14px;
+        margin: 20px 0 20px 130px;
+        letter-spacing: 0.25px;
+    }
+
+    .black {
+        background-color: black;
+        width: 140px;
+        padding: 3px 0 3px 0;
+        margin: 5px;
+        border: none;
+        border-radius: 5px;
+        text-align: left;
+        line-height: none;
+        position: relative;
+        left: 20px;
+    }
+
+    .black a {
+        color: white;
+        font-family: "Roboto", sans-serif;
+        text-decoration: none;
+    }
+
+    #firstLineApple {
+        font-size: 10px;
+        letter-spacing: 0.5px;
+        
+    }
+
+    #secondLineApple {
+        font-size: 18px;
+        letter-spacing: 0.5px;
+    }
+
+    #firstLinePlay {
+        font-size: 9px;
+    }
+
+    #secondLinePlay {
+        font-size: 16px;
+    }
+
+    .icon {
+        width: 30px;
+        float: left;
+        padding-left: 5px;
+        padding-right: 5px;
     }
 
    
